70630a407574298d29e88eb8edf857ce1fec51b1
Jorja Hung <jorjahung@gmail.com>
2014-01-28 18:46:42 +0000

    Refactored

--- lib/grid.rb
+++ lib/grid.rb
@@ -21,17 +21,29 @@ class Grid
 	end
 
 	def own_box(cell_index)
-		box_row_number = (cell_index / ROWS_MAX_SIZE) / BOX_SIZE * BOX_SIZE
-		min_box_column_number = (cell_index % ROWS_MAX_SIZE) / BOX_SIZE * BOX_SIZE
-		max_box_column_number = (cell_index % ROWS_MAX_SIZE) / BOX_SIZE * BOX_SIZE + 2
-
-			box = []
-			box << rows[box_row_number][min_box_column_number..max_box_column_number].map {|cell| cell.value}.select {|cell| cell > 0}
-			box << rows[box_row_number + 1][min_box_column_number..max_box_column_number].map {|cell| cell.value}.select {|cell| cell > 0}
-			box << rows[box_row_number + 2][min_box_column_number..max_box_column_number].map {|cell| cell.value}.select {|cell| cell > 0}
-			box.flatten
-
+		build_box_with(box_row_number_with(cell_index), min_box_column_number(cell_index), max_box_column_number(cell_index))
 	end
+
+	def build_box_with(box_row_number, min_box_column_number, max_box_column_number)
+		[0,1,2].map{|index| calculate_box_row_with(index, box_row_number, min_box_column_number, max_box_column_number)}.flatten
+	end
+
+	def calculate_box_row_with(index, box_row_number, min_box_column_number, max_box_column_number)
+		rows[box_row_number + index][min_box_column_number..max_box_column_number].map {|cell| cell.value}.select {|cell| cell > 0}
+	end
+
+	def box_row_number_with(cell_index)
+		(cell_index / ROWS_MAX_SIZE) / BOX_SIZE * BOX_SIZE
+	end
+
+	def min_box_column_number(cell_index)
+		(cell_index % ROWS_MAX_SIZE) / BOX_SIZE * BOX_SIZE
+	end
+
+	def max_box_column_number(cell_index)
+		(cell_index % ROWS_MAX_SIZE) / BOX_SIZE * BOX_SIZE + 2
+	end
+
 
 	def rows
 		cells.each_slice(9).to_a
